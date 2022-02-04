7e4c3fb9c6a131969793cc27b8416963600133c4
AMwa1 <98977562+AMwa1@users.noreply.github.com>
2022-02-04 11:55:55 -0500

    Update and rename tomfooleryV1.js to tomfooleryV1-1.js
    Fixed worker issue, trying out new icon thing.

--- tomfooleryV1-1.js
+++ tomfooleryV1-1.js
@@ -9,17 +9,17 @@ G.AddData({
         new G.Res({
            name:'gun',
            desc: 'pew',
-           icon:[5,5],
+           icon:'https://wwwassets.rand.org/content/rand/research/gun-policy/key-findings/what-science-tells-us-about-the-effects-of-gun-policies/jcr:content/par/teaser_copy_copy.aspectfit.0x1200.jpg/1557869169124.jpg',
            displayUsed: true,
            category:'gear'
         });
         
         // Augment base data for Artisan.
-        G.getDict('blacksmith workshop').modes['make gun']={name:'Make guns',icon:[5,5],desc:'Use 3 [hard metal ingot]s to make 1 [gun].',req:{'gun smithing':true}, use:{'metal tools':1}};
+        G.getDict('blacksmith workshop').modes['make gun']={name:'Make guns',icon:[5,5],desc:'Use 3 [hard metal ingot]s to make 1 [gun].',req:{'gun smithing':true}, use:{'worker':1,'metal tools':1}};
         G.getDict('blacksmith workshop').effects.push({type:'convert', from:{'hard metal ingot':3}, into:{'gun':1}, every:6,mode:'make gun'});
         
         // Augment base data for Hunter.
-        G.getDict('hunter').modes['shoot']={name:'Shoot',icon:[5,5],desc:'Use [gun]s to shoot prey.',use:{'gun':1}};
+        G.getDict('hunter').modes['shoot']={name:'Shoot',icon:[5,5],desc:'Use [gun]s to shoot prey.',use:{'worker':1,'gun':1}};
         G.getDict('hunter').effects.push({type:'gather',context:'hunt',amount:7,max:9,mode:'shoot'});
         
         // Add new tech.
