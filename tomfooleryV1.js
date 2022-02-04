a2709bf1706cad28ae3868adb736ea2555f822eb
AMwa1 <98977562+AMwa1@users.noreply.github.com>
2022-02-04 09:20:07 -0500

    tomfooleryV1, guns fully functional
    Changed icons, blacksmiths now make the guns.

--- tomfooleryV1.js
+++ tomfooleryV1.js
@@ -9,25 +9,25 @@ G.AddData({
         new G.Res({
            name:'gun',
            desc: 'pew',
-           icon:[9,5],
+           icon:[5,5],
            displayUsed: true,
            category:'gear'
         });
         
         // Augment base data for Artisan.
-        G.getDict('artisan').modes['make gun']={name:'Make guns',desc:'Use 3 [hard metal ingot]s to make 1 [gun].',req:{'gun smithing':true}, use:{'metal tools':1}};
-        G.getDict('artisan').effects.push({type:'convert', from:{'hard metal ingot':3}, into:{'gun':1}, every:6,mode:'make gun'});
+        G.getDict('blacksmith workshop').modes['make gun']={name:'Make guns',icon:[5,5],desc:'Use 3 [hard metal ingot]s to make 1 [gun].',req:{'gun smithing':true}, use:{'metal tools':1}};
+        G.getDict('blacksmith workshop').effects.push({type:'convert', from:{'hard metal ingot':3}, into:{'gun':1}, every:6,mode:'make gun'});
         
         // Augment base data for Hunter.
-        G.getDict('hunter').modes['shoot']={name:'Shoot',desc:'Use [gun]s to shoot prey.',use:{'gun':1}};
+        G.getDict('hunter').modes['shoot']={name:'Shoot',icon:[5,5],desc:'Use [gun]s to shoot prey.',use:{'gun':1}};
         G.getDict('hunter').effects.push({type:'gather',context:'hunt',amount:7,max:9,mode:'shoot'});
         
         // Add new tech.
         new G.Tech({
            name:'gun smithing',
-           desc:'[artisan]s can now make [gun]s. Unlocks a new mode for @[hunter].',
+           desc:'[blacksmith workshop]s can now make [gun]s. Unlocks a new mode for @[hunter].',
            icon:[9, 5],
-           cost:{'insight':25},
+           cost:{'insight':50},
            req:{'iron-working':true, 'bows':true}
         });
     }
