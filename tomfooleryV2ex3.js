05312c6aa8742a3d5cd7bc2e340f840d3b44b7c1
AMwa1 <98977562+AMwa1@users.noreply.github.com>
2022-02-04 18:57:10 -0500

    Update and rename tomfooleryV2ex21.js to tomfooleryV2ex3.js

--- tomfooleryV2ex3.js
+++ tomfooleryV2ex3.js
@@ -15,7 +15,7 @@ G.AddData({
            category:'gear'
         });
         
-        /*new G.Res({
+        new G.Res({
            name:'ash',
            desc:'Ashes of people who were cremated. Produces some [faith].',
            icon:[12,8],
@@ -26,7 +26,7 @@ G.AddData({
                var spent = G.lose(me.name, randomFloor(toSpoil), 'decay');
                G.pseudoGather(G.getRes('faith'), randomFloor(spent));
            };
-        });*/
+        });
         
         // Augment base data for Artisan.
         G.getDict('blacksmith workshop').modes['make gun']={name:'Make guns',icon:[5,5],desc:'Use 3 [hard metal ingot]s to make 1 [gun].',req:{'gun smithing':true}, use:{'worker':1,'metal tools':1}};
@@ -36,7 +36,11 @@ G.AddData({
         G.getDict('hunter').modes['shoot']={name:'Shoot',icon:[5,5],desc:'Use [gun]s to shoot prey.',use:{'worker':1,'gun':1}};
         G.getDict('hunter').effects.push({type:'gather',context:'hunt',amount:7,max:9,mode:'shoot'});
         
-        // Add new tech.
+        // Augment base data for Firekeeper.
+        G.getDict('firekeeper').modes['cremate']={name:'Cremate corpses',icon:[9,5],desc:'Turn [corpse]s into [ash]',use:{'worker':1,'fire pit':1}};
+        G.getDict('firekeeper').effects.push({type:'convert',from:{'corpse':1}, into:{'ash':5}, every:7,mode:'cremate'});
+        
+        // Add new techs.
         new G.Tech({
            name:'gun smithing',
            desc:'[blacksmith workshop]s can now make [gun]s. Unlocks a new mode for @[hunter].',
@@ -44,5 +48,12 @@ G.AddData({
            cost:{'insight':50},
            req:{'iron-working':true, 'bows':true}
         });
+        
+        new G.Tech({
+            name:'cremation',
+            desc:'[firekeeper]s can now cremate [corpse]s, turning them into [ash].',
+            icon:[8,3],
+            cost:{'insight':25,'faith':2},
+            req:{'burial':true, 'fire-making':true, 'fear of death':true}
     }
 });
