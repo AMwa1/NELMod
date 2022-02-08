789953c8c0f28a00a453c669c46e1d2771833e32
AMwa1 <98977562+AMwa1@users.noreply.github.com>
2022-02-08 10:51:05 -0500

    Update and rename tomfooleryV1-6.js to tomfooleryV1-7.js

--- tomfooleryV1-7.js
+++ tomfooleryV1-7.js
@@ -4,7 +4,7 @@ G.AddData({
     desc:'A mod that engages in a high level of tomfoolery.',
     engineVersion:1,
     manifest:'ModManifest.js',
-    requires:'Default dataset*',
+    requires:['Default dataset*'],
     func:function(){
         // Add new resources.
         new G.Res({
