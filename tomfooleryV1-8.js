70c73aa3fed63a48e999c5020b415faa2ef357da
AMwa1 <98977562+AMwa1@users.noreply.github.com>
2022-02-09 10:49:59 -0500

    Update and rename tomfooleryV1-7.js to tomfooleryV1-8.js
    Experimental for V2

--- tomfooleryV1-8.js
+++ tomfooleryV1-8.js
@@ -7,6 +7,7 @@ G.AddData({
     requires:['Default dataset*'],
     func:function(){
         // Add new resources.
+        // Gun resource, a type of gear that can be used for hunting and obtained via blacksmiths.
         new G.Res({
            name:'gun',
            desc: 'pew',
@@ -15,15 +16,22 @@ G.AddData({
            category:'gear'
         });
         
-        // Augment base data for Artisan.
+        // Augment base data for Blacksmith Workshop to allow for gun smithing.
         G.getDict('blacksmith workshop').modes['make gun']={name:'Make guns',icon:[5,5],desc:'Use 3 [hard metal ingot]s to make 1 [gun].',req:{'gun smithing':true}, use:{'worker':1,'metal tools':1}};
         G.getDict('blacksmith workshop').effects.push({type:'convert', from:{'hard metal ingot':3}, into:{'gun':1}, every:6,mode:'make gun'});
         
-        // Augment base data for Hunter.
+        // Augment base data for Hunter to add shoot to hunt mode.
         G.getDict('hunter').modes['shoot']={name:'Shoot',icon:[5,5],desc:'Use [gun]s to shoot prey.',use:{'worker':1,'gun':1}};
         G.getDict('hunter').effects.push({type:'gather',context:'hunt',amount:7,max:9,mode:'shoot'});
         
+        // Augment base data for Woodcutter to allow use of stone and metal tools, not just knapped tools.
+        G.getDict('woodcutter').modes['advanced cut wood']={name:'Advanced Cut Wood',icon:[0,0],desc:'Use [stone tool]s to cut down trees.',use:{'worker':1,'stone tools':1},req:{'tool-making':true}};
+        G.getDict('woodcutter').modes['advanceder cut wood']={name:'Better Advanced Cut Wood',icon:[0,0],desc:'Use [metal tool]s to cut down trees.',use:{'worker':1,'metal tools':1},req:{'smelting':true}};
+        G.getDict('woodcutter').effects.push({type:'gather',context:'chop',amount:2,max:2,mode:'advanced cut wood'});
+        G.getDict('woodcutter').effects.push({type:'gather',context:'chop',amount:3,max:3,mode:'advanceder cut wood'});
+        
         // Add new techs.
+        // Gun smithing tech, allows blacksmiths to make guns.
         new G.Tech({
            name:'gun smithing',
            desc:'[blacksmith workshop]s can now make [gun]s. Unlocks a new mode for @[hunter].',
